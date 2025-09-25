299aecf012282926c17371c55a951a6a68e2a0d2
Felix Allard <xilef992@gmail.com>
2025-09-25 12:23:05 -0400

    Some comments

--- ArduinoMaze.c
+++ ArduinoMaze.c
@@ -11,21 +11,37 @@
 #define COLS 7
 
 _Bool tileMap[ROWS][COLS] = {0}; //All variable initialized as false
+///@author Felix Allard
+///@brief {0 = Left}
+///{1 = Top ( Straight )}
+///{2 = Right}
+///{3 = down}
+///@attention 1 is Straight
+///@name Facing Direction
+int facingDirection = 1;
 
 
+///@author Felix
+///@brief The function called before everything else. This is called by the C++ Initializer
+///@file {main.cpp}
 void SetupMaze() {
     BoardInit();
 }
 
-
+///@def The loop function of Arduino
+///@brief A function that is always looped in the robot
 void LoopMaze() {
 
 }
 
+///@brief A function that goes straight
 void Advance() {
 
 }
-void Turn() {
+
+///
+/// @param directionToFaceAfter Where the Robot will be facing after
+void Turn(int directionToFaceAfter) {
 
 }
 
